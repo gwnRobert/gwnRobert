9a4c5240ab646255f7a8ebb25d8128ae4592eb46
Robert Van 't Hul <124918225+gwnRobert@users.noreply.github.com>
2023-03-22 14:04:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-var typed = new Typed(".multiple-text", {strings: ["Frontend Developer", "Backend Developer", "Fullstack Developer"], typeSpeed: 100, backSpeed: 100, backDelay: 1000, loop: true})
+var typed = new Typed(".multiple-text", {strings: ["Frontend Developer", "Backend Developer", "Fullstack Developer", "Student"], typeSpeed: 100, backSpeed: 100, backDelay: 1000, loop: true})
